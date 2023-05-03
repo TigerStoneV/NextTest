4db1349ae222aa3656f49c22726c1da6ec489caa
ddang_gu@naver.com <ddang_gu@naver.com>
2023-05-03 14:38:33 +0900

    review a payments on next with typescript
    review a payments on next with typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,31 @@
 import { loadTossPayments } from "@tosspayments/payment-sdk";
 import styled from "styled-components";
-const Button = styled.button`
-  font-size: 15px;
-`;
-const Home = () => {
-  const handleClick = async () => {
-    const tossPayments = await loadTossPayments(
-      process.env.NEXT_PUBLIC_TOSS_CLIENT_KEY || ""
-    );
 
-    await tossPayments.requestPayment("카드", {
-      amount: 10000,
-      orderId: Math.random().toString(36).slice(2),
-      orderName: "스타벅스",
-      successUrl: `${window.location.origin}/api/payments`,
-      failUrl: `${window.location.origin}/api/payments/fail`,
-      // customerEmail: "customer123@gmail.com",
-      customerName: "김희연",
-    });
-  };
-  return (
-    <>
-      <h1>
-        <Button onClick={handleClick}>인호 10원</Button>
-      </h1>
-    </>
-  );
+const Button = styled.button`
+    font-size: 15px;
+`;
+
+const Home = () => {
+    const handleClick = async () => {
+        const tossPayments = await loadTossPayments(process.env.NEXT_PUBLIC_TOSS_CLIENT_KEY || "");
+
+        await tossPayments.requestPayment("카드", {
+            amount: 4500,
+            orderId: Math.random().toString(36).slice(2),
+            orderName: "스타벅스",
+            successUrl: `${window.location.origin}/api/success`,
+            failUrl: `${window.location.origin}/api//fail`,
+            // customerEmail: "customer123@gmail.com",
+            customerName: "구매자이름",
+        });
+    };
+    return (
+        <>
+            <h1>
+                <Button onClick={handleClick}>스타벅스 아메리카노</Button>
+            </h1>
+        </>
+    );
 };
 
 export default Home;
--- src/pages/payments/complete/page.tsx
+++ src/pages/payments/complete/page.tsx
@@ -1,48 +1,51 @@
-import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 export default function Page() {
-  const [payment, setPayment] = useState<any>();
-  const [id, setId] = useState<any>();
-  const secretKey = process.env.NEXT_PUBLIC_TOSS_SECRET_KEY || "";
-  const basicToken = Buffer.from(`${secretKey}:`, "utf-8").toString("base64");
-  function searchParam(key: any) {
-    return new URLSearchParams(location.search).get(key);
-  }
+    const [payment, setPayment] = useState<any>();
+    const [id, setId] = useState<any>();
+    const secretKey = process.env.NEXT_PUBLIC_TOSS_SECRET_KEY || "";
+    const basicToken = Buffer.from(`${secretKey}:`, "utf-8").toString("base64");
+    function searchParam(key: any) {
+        return new URLSearchParams(location.search).get(key);
+    }
 
-  useEffect(() => {
-    const orderId = searchParam("orderId");
+    useEffect(() => {
+        const orderId = searchParam("orderId");
 
-    setId(orderId);
-    (async () => {
-      const url = `https://api.tosspayments.com/v1/payments/orders/${id}`;
+        setId(orderId);
+        (async () => {
+            const url = `https://api.tosspayments.com/v1/payments/orders/${id}`;
 
-      const payments = await fetch(url, {
-        headers: {
-          Authorization: `Basic ${basicToken}`,
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => res.json())
-        .catch((err) => {
-          console.log(err);
-        });
+            const payments = await fetch(url, {
+                headers: {
+                    Authorization: `Basic ${basicToken}`,
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((res) => res.json())
+                .catch((err) => {
+                    console.log(err);
+                });
 
-      setPayment(payments);
-    })();
-  });
+            setPayment(payments);
+        })();
+        console.log(payment, "1");
+    });
 
-  return (
-    <div>
-      {payment && (
+    return (
         <div>
-          <h1>결제가 완료되었습니다</h1>
-          <ul>
-            <li>결제 상품 {payment.orderName}</li>
-            <li>주문번호 {payment.orderId} </li>
-          </ul>
+            {payment && (
+                <div>
+                    <h1>결제가 완료되었습니다</h1>
+                    <ul>
+                        <li>결제 상품 {payment.orderName}</li>
+                        <li>주문번호 {payment.orderId} </li>
+                        <li> 총금액 : {payment.totalAmount} </li>
+                        <li>요청일시: {payment.requestedAt} </li>
+                        <li>부가세 : {payment.vat}</li>
+                    </ul>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
